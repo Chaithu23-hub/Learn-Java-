1c7c3bd86a8351e2a9556e80a610f642efa77670
ChaithuCheepati <chepatichaithravardhanreddy@gmail.com>
2025-06-14 23:06:28 +0530

    Create FuncWithNoArg.java

---
+++ basics/functions/FuncWithNoArg.java
@@ -0,0 +1,10 @@
+class FuncWithNoArg {
+    //  code here
+    public static void helloFunction(){
+        System.out.println("Hello");
+    }
+    public static void main(String[] args){
+        helloFunction();
+    }
+}
+//output hello
