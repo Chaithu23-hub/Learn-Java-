287f322ced61cfd067d4dce05ef08a3094f32272
ChaithuCheepati <chepatichaithravardhanreddy@gmail.com>
2025-06-14 23:09:12 +0530

    Create FuncWithArg.java

---
+++ basics/functions/FuncWithArg.java
@@ -0,0 +1,12 @@
+class FuncWithArg {
+    // Method to take two arguments a and b and return their sum
+    public static int argumentFunction(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        // Example usage of the function
+        int result = argumentFunction(5, 3);
+        System.out.println("Sum: " + result);
+    }
+}
