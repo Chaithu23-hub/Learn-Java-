67e566d96c4bfcfb857e8368b41902b3bcea3a20
ChaithuCheepati <chepatichaithravardhanreddy@gmail.com>
2025-06-15 23:25:23 +0530

    Create DecrementArrayElements.java

---
+++ basics/Arrays/DecrementArrayElements.java
@@ -0,0 +1,9 @@
+class DecrementArrayElements {
+    public static int[] decrementArrayElements(int[] arr) {
+        // Code here
+        for(int i=0;i<arr.length;i++){
+            arr[i]--;
+        }
+        return arr;
+    }
+}
