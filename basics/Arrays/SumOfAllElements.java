0d7ba7a22283b464247f9cba681dca5a0285d8a5
ChaithuCheepati <chepatichaithravardhanreddy@gmail.com>
2025-06-15 23:38:36 +0530

    Create SumOfAllElements.java

---
+++ basics/Arrays/SumOfAllElements.java
@@ -0,0 +1,10 @@
+class SumOfAllElements {
+    public static int arraySum(int[] arr) {
+        // code here
+        int sum=0;
+        for(int n:arr){
+            sum=sum+n;
+        }
+        return sum;
+    }
+}
