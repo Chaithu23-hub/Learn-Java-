2ca5f73c724ca5ac44b4e1975ec882ecb2d69e3f
ChaithuCheepati <chepatichaithravardhanreddy@gmail.com>
2025-06-20 20:29:43 +0530

    Update MajorityElement.java

--- interview_prep/MajorityElement.java
+++ interview_prep/MajorityElement.java
@@ -1,15 +1,22 @@
-public class Solution {
+class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0, candidate = 0;
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
+        int maj=nums[0];
+        int count=1;
+        int n=nums.length;
+        for(int i=1;i<n;i++){
+            if(nums[i]==maj){
+                count++;
+            }else{
+                count--;
+                if(count==0){
+                    maj=nums[i];
+                    count=1;
+                }
             }
-            count += (num == candidate) ? 1 : -1;
         }
-        return candidate;
+        return maj;
+        }
     }
-}
 /*
 169. Majority Element
 Solved
